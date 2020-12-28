a33eac8b6eab83b2d9dac281813f07d2db2d5d9b
liuba-larousse <liubovkapitulskaya@gmail.com>
2020-12-28 03:12:29 -0600

    06, 08, 09, 10, 11, 12 scc changes

--- 06_weatherapp_API/app.js
+++ 06_weatherapp_API/app.js
@@ -1,50 +1,60 @@
-const express = require("express");
-const https = require("http");
-const bodyParser = require("body-parser");
+/** @format */
+
+const express = require('express');
+const https = require('http');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.post("/", function (req, res) {
+app.post('/', function (req, res) {
   console.log(req.body.cityName);
   const query = req.body.cityName;
-  const apiKey = "e0d72a4fa58f166403a644876debed41";
-  const units = "metric";
+  const apiKey = 'e0d72a4fa58f166403a644876debed41';
+  const units = 'metric';
   const url =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    'http://api.openweathermap.org/data/2.5/weather?q=' +
     query +
-    "&appid=" +
+    '&appid=' +
     apiKey +
-    "&units=" +
+    '&units=' +
     units;
 
   https.get(url, function (response) {
     console.log(response.statusCode);
 
-    response.on("data", function (data) {
+    response.on('data', function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
+      const weatherDescription =
+        weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      res.write("<p>The weather is currently <p>" + weatherDescription);
+      const imageURL =
+        'http://openweathermap.org/img/wn/' +
+        icon +
+        '@2x.png';
       res.write(
-        "<h1>The temperature in " +
-          query +
-          " is " +
-          temp +
-          " degrees Celcius.</h1>"
+        '<p>The weather is currently <p>' +
+          weatherDescription
       );
-      res.write("<img src=" + imageURL + ">");
+      res.write(
+        '<h1>The temperature in ' +
+          query +
+          ' is ' +
+          temp +
+          ' degrees Celcius.</h1>'
+      );
+      res.write('<img src=' + imageURL + '>');
       res.send();
     });
   });
 });
+
 /*
 const query = "London"
 const apiKey = "e0d72a4fa58f166403a644876debed41"
@@ -71,5 +81,5 @@ https.get(url, function (response) {
 */
 
 app.listen(3000, function () {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
--- 12_secret_door/app.js
+++ 12_secret_door/app.js
@@ -1,34 +1,43 @@
-const pressed = [];
-const secretCode = "java";
+/** @format */
 
-var currentDiv = document.querySelector(".main-div");
+const pressed = [];
+const secretCode = 'java';
+
+var currentDiv = document.querySelector('.main-div');
 
 function addElement() {
-  var newElement = document.createElement("h1");
+  var newElement = document.createElement('h1');
   var newContent = document.createTextNode(
-    "You have opened a secret path...(this is javascript text)"
+    'You have opened a secret path...(this is javascript text)'
   );
-  newElement.style.color = "#ff847c";
+  newElement.style.color = '#3282b8';
+  newElement.style.fontWeight = '900';
   newElement.appendChild(newContent);
-  currentDiv.insertAdjacentElement("afterbegin", newElement);
+  currentDiv.insertAdjacentElement(
+    'afterbegin',
+    newElement
+  );
 }
 
 function addImage() {
-  var img = document.createElement("img");
-  img.setAttribute("src", "image/door.jpg");
-  img.setAttribute("alt", "na");
-  img.setAttribute("height", "300px");
-  img.setAttribute("width", "200px");
+  var img = document.createElement('img');
+  img.setAttribute('src', 'image/door.jpg');
+  img.setAttribute('alt', 'na');
+  img.setAttribute('height', '600px');
+  img.setAttribute('width', '400px');
   document.body.appendChild(img);
-  currentDiv.insertAdjacentElement("afterbegin", img);
+  currentDiv.insertAdjacentElement('afterbegin', img);
 }
 
-window.addEventListener("keyup", (e) => {
+window.addEventListener('keyup', (e) => {
   console.log(e.key);
   pressed.push(e.key);
-  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-  if (pressed.join("").includes(secretCode)) {
-    console.log("Ding Ding");
+  pressed.splice(
+    -secretCode.length - 1,
+    pressed.length - secretCode.length
+  );
+  if (pressed.join('').includes(secretCode)) {
+    console.log('Ding Ding');
     addElement();
     addImage();
   }
